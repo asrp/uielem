b8dacd9205af97edf0b915036abed9ed3e024c9d
asrp <asrp@email.com>
2017-02-18 06:52:31 +0100

    Add barebones setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='uielem',
+      version='0.2',
+      description='Wrapper over Tkinter for more Pythonic UI building',
+      long_description='Wrapper over Tkinter for more Pythonic UI building',
+      url='https://github.com/asrp/uielem',
+      author='asrp',
+      author_email='asrp@email.com',
+      py_modules=['uielem'],
+      keywords='wrapper tkinter pythonic')
